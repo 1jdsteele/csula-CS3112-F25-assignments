9fbd7361a3e9f766af388fe4f028c462851f8d01
Jake Steele <jsteele@Jakes-MacBook-Air.local>
2025-09-01 18:21:07 -0700

    cleaned up insertion sort

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,33 +1,18 @@
 import random
-from module import generate_rand_array, selection_sort
+from module import generate_rand_array, selection_sort, insertion_sort
 
-print("hello cruel world")
-
+# introduction statement
+print(
+    "Welcome to Jake's assignement number 2 for analysis of algorithms. In this assignement we will sort randomly aorted arrays by two different methods. The arrays can have a length 2-10 and can include any number 0-100.\n"
+)
 
 rand_arr1 = generate_rand_array()
-rand_arr2 = generate_rand_array()
-
-
 print("array 1:", rand_arr1)
-print("array 2:", rand_arr2)
-
-
 rand_arr1 = selection_sort(rand_arr1)
-print("array 1 after selection sort:", rand_arr1)
+print("array 1 after selection sort:", rand_arr1, "\n")
 
 
-# now use insertion sort to sort second array
-for i in range(len(rand_arr2)):
-    # we have a window that is sorted from 0 to i - 1 and we are inserting the i - 1 position
-    for j in range(i):
-        if rand_arr2[i] < rand_arr2[j]:
-            temp = rand_arr2[i]
-            for k in range(i, j, -1):
-                rand_arr2[k] = rand_arr2[k - 1]
-                # print("if statement k looping, arr 2 rn: ", rand_arr2)
-            rand_arr2[j] = temp
-            # print("if statement hit done. array 2 rn: ", rand_arr2)
-    # next element: insert into correct spot of sorted section
-    # continue growing sorted section and inserting in correct place
-
-print("array 2 after insertion sort:", rand_arr2)
+rand_arr2 = generate_rand_array()
+print("array 2:", rand_arr2)
+rand_arr2 = insertion_sort(rand_arr2)
+print("array 2 after insertion sort:", rand_arr2, "\n")
--- assignment2/module.py
+++ assignment2/module.py
@@ -3,15 +3,16 @@ import random
 
 def generate_rand_array() -> list[int]:
     arr = []
-    rand_size = random.randint(2, 10)
 
-    for i in range(0, rand_size):
+    for i in range(0, random.randint(2, 10)):
         arr.append(random.randint(2, 100))
 
     return arr
 
 
-def selection_sort(arr: list[int]) -> list[int]:
+# notes: this is unstable - as in it reorders same value elements
+# to fix: romove the min and insert it at i, shifting all else
+def selection_sort(arr: list[int]) -> list[int]:  # O(n^2)
     for i in range(len(arr)):
         curr_min = arr[i]
         curr_min_index = i
@@ -20,4 +21,16 @@ def selection_sort(arr: list[int]) -> list[int]:
                 curr_min = arr[j]
                 curr_min_index = j
         arr[i], arr[curr_min_index] = arr[curr_min_index], arr[i]
+    return arr
+
+
+def insertion_sort(arr: list[int]) -> list[int]:  # O(n^3)
+    for i in range(len(arr)):
+        for j in range(i):
+            if arr[i] < arr[j]:
+                temp = arr[i]
+                for k in range(i, j, -1):
+                    arr[k] = arr[k - 1]
+                arr[j] = temp
+                break
     return arr
